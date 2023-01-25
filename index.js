957cec672d088e24be47dfe25f0bab07d06bf3f8
Dare Jamiu <jammy190@gmail.com>
2023-01-25 07:33:17 +0100

    random number generator

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 
 // const randomElement = array[Math.floor(Math.random() * array.length)]; syntax
-// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",
-// "Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y",
-// "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_",
-// "-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-// "/"]
 
 
-
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-+={[}],|:;<>.?/".split("")
 // display specific length of password
 // a specific amount of numbers, letter and symbol in each password
 // copy to clipboard button
 
 
-const generateBtn = document.getElementById("generate-btn")
-let displayCharacter = document.getElementById("displayy")
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-+={[}],|:;<>.?/"
+let generateBtn = document.getElementById("generate-btn")
+let copyBtn = document.getElementById("copy-btn")
+let passwordLength = 10
 
-// function generatePassword() {
-//     const randomIndex =  characters[Math.floor(Math.random() * characters.length)] 
-//     console.log(randomIndex)
-//     displayCharacter.textContent = randomIndex
-// }
-
-
-generateBtn.addEventListener("click", function generatePassword() {
+function generatePassword() {
     
-    for (let i = 0; i < 10; i++) {
-        let randomIndex =  characters[Math.floor(Math.random() * characters.length)] 
-        // let randomIndex =  Math.floor(Math.random() * characters.length)
-        displayCharacter.textContent = randomIndex
-    }
-})
+    let password = ""
 
+    for (let i = 0; i <= passwordLength; i++) {
+        
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters.substring(randomIndex, randomIndex +1)
+    }
+    document.getElementById("output").value = password
+}
+
+
+// copyPassword() {
+//     let output = document.getElementById("output")
+//     output.select()
+//     output.setSelectionRange(0,999)
+//     document.execCommand("copy")
+// }
